945ca6c8772828113ec88318117f3842a50da6cd
valcelli <sara.valcelli@campusdavinci.edu.it>
2025-10-16 09:48:08 +0200

    Add files via upload

---
+++ punti_cartesiano 3.py
@@ -0,0 +1,53 @@
+#dato un insime di punti su un piano cartesiano,
+#random compresi tra l'intervallo 0,10. Calcolare il
+#punto con ascissa massima e con ordinata minim.
+
+import random
+x=[]
+y=[]
+for i in range(0,20):
+    x.append(random.randint(0,10))
+    y.append(random.randint(0,10))
+
+    
+#ascissa massima,scorrere la lista delle x, trovare il massimo e salvare l'indice
+#visualizzare poi con un print il numero
+massimo=x[0]
+for i in range(1,20):
+    if x[i]>massimo:
+        massimo=x[i]
+        indice_massimo=i
+print(massimo,y[indice_massimo])
+
+
+massimo=y[0]
+for i in range(1,20):
+    if y[i]>massimo:
+        massimo=y[i]
+        indice_massimo=i
+print(massimo,x[indice_massimo])
+    
+#le tuple sono numeri raggruppati nelle parentisi tonde
+
+punti_cartesiano=[]
+for i in range(0,20):
+    punto=(random.randint(0,10),random.randint(0,10))
+    punti_cartesiano.append(punto)
+    
+massimo=punti_cartesiano[0]
+if i in range(1,20):
+    if punti_cartesiano[i][0]>massimo[0]:
+        massimo = punti_cartesiano[i]
+print(massimo)
+
+minimo=punti_cartesiano[0]
+if i in range(1,20):
+    if punti_cartesiano[i][1]<minimo[1]:
+        minimo = punti_cartesiano[i]
+print(minimo)
+    
+#come accedere alla x del primo punto:
+print (punti_cartesiano[0][0])
+#come accedere alla y del primo punto:
+print (punti_cartesiano[0][1]) 
+        
