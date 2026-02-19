10e94ee45e9edc0d373992ca7987b300a92f6111
valcelli <sara.valcelli@campusdavinci.edu.it>
2026-02-19 09:57:38 +0100

    Add files via upload

--- insertion Sort.py
+++ insertion Sort.py
@@ -8,6 +8,13 @@ if lista[1]<lista[0]:
     lista[1]=lista[0]
     lista[0]=toorder
 print(lista)
+#[11,12,13,5,6]
+toorder=lista[2]
+for i in range(0,2):
+    if lista[i]>lista[1]:
+        lista[i]=lista[1]
+        lista[1]=toorder
+print(lista)
     
 
 
